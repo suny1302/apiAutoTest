d739d1ea4827c224921baf1499cb52fb38b1376a
suny1302 <suny1302@163.com>
2022-03-23 23:41:14 +0800

    提交练习

---
+++ mytestng/src/main/java/com/suny/testng/TestngAnnotion.java
@@ -0,0 +1,12 @@
+package com.suny.testng;
+
+import org.testng.annotations.Test;
+
+public class TestngAnnotion {
+
+
+    @Test
+    public void test01(){
+        System.out.println("Hello,World!");
+   }
+}
